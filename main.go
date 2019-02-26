25af8dc47aaf299cf3f461e206c377d78a84626c
gumpen <shuhei1993727@gmail.com>
2019-02-27 01:10:29 +0900

    Finish

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		log.Fatal("no args")
+		os.Exit(1)
+	}
+
+	runes := []rune(args[0])
+	for _, r := range runes {
+		var byteString []string
+		for _, b := range []byte(string(r)) {
+			byteString = append(byteString, fmt.Sprintf("%x", b))
+		}
+
+		fmt.Printf("%s", string(r))
+		fmt.Print("|-|")
+		fmt.Printf("%d", r)
+		fmt.Print("|-|")
+		fmt.Printf("%s\n", strings.Join(byteString, "|"))
+	}
+
 }
